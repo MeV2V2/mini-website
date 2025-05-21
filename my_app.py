3ba5161d8c9fac0ec07f6957f973790c0c22bbb3
MeV2V2 <jaeminp707@gmail.com>
2025-05-21 15:20:07 +1000

    added git ignore and updates to app

--- my_app.py
+++ my_app.py
@@ -3,9 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-
 def home():
-    return "Hello, World!"
+    return "<h1>HOME PAGE</h1>"
+
+@app.route("/hello-world")
+def hello():
+    return "hello world!"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
